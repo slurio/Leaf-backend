65d0e2a00beda6ddecee43f454328ff62861626b
slurio <samantha.lurio@gmail.com>
2020-10-29 22:05:37 -0400

    Item controller create and index actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,51 @@
+require 'json'
+require 'net/https'
+
+require "google/cloud/vision"
+
+
 class ItemsController < ApplicationController
+
+    def index
+        items = Item.all
+        
+        render json: items
+    end
+
+    def create
+        front_tag_img = params['item']['front_tag_img']
+        back_tag_img = params['item']['back_tag_img']
+
+        image_annotator = Google::Cloud::Vision.image_annotator
+        byebug
+
+        # key="870893b239264d61f86aa54532d394c3d845d915"
+        # url="https://vision.googleapis.com/v1/images:annotate?key=#{key}"
+
+        # body = {
+        #     requests: [{
+        #       image: {
+        #         content: front_tag_img
+        #       },
+        #       features: [
+        #         {
+        #           type: 'TEXT_DETECTION', # Details are below.
+        #           maxResults: 3 # The number of results you would like to get
+        #         }
+        #       ]
+        #     }]
+        #   }
+
+        #     uri = URI.parse(API_URL)
+        #     https = Net::HTTP.new(uri.host, uri.port)
+        #     https.use_ssl = true
+        #     request = Net::HTTP::Post.new(uri.request_uri)
+        #     request["Content-Type"] = "application/json"
+        #     response = https.request(request, body.to_json)
+    end
+
+    private
+    def item_params
+        params.require(:item).permit(:title, :user_id, :front_tag_img, :back_tag_img)
+    end
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :title, :front_tag_img, :back_tag_img, :fiber_facts
+  attributes :id, :title, :front_tag_img, :back_tag_img, :fiber_facts, :user
   
   def fiber_facts
     ActiveModel::SerializableResource.new(object.fiber_facts,  each_serializer: FiberFactSerializer)
